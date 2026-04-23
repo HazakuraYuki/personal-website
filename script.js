5ae6ab640fe67a7eaea3be886c221bca75c07499
Ludevica Ayuningtyas Susanto <susantoludevica@gmail.com>
2026-04-23 14:34:54 +0700

    update 2

--- script.js
+++ script.js
@@ -1,3 +1,20 @@
 function sayHello() {
     alert("Halo! Selamat datang 😄");
 }
+
+// ANIMASI SCROLL
+const faders = document.querySelectorAll('.fade-in');
+
+window.addEventListener('scroll', () => {
+    faders.forEach(el => {
+        const rect = el.getBoundingClientRect();
+        if (rect.top < window.innerHeight - 100) {
+            el.classList.add('show');
+        }
+    });
+});
+
+// NAVBAR TOGGLE
+function toggleMenu() {
+    document.getElementById("menu").classList.toggle("active");
+}
